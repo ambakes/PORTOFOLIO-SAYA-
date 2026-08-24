c9738a0f426c42f0d47897b08a0a4faac491df69
ambakes <jhonatanfarles@gmail.com>
2026-08-24 21:05:45 +0800

    first commit

--- script.js
+++ script.js
@@ -93,5 +93,5 @@ document.addEventListener("DOMContentLoaded", () => {
             updateThemeUI(isLight);
             localStorage.setItem('theme', isLight ? 'light' : 'dark');
         });
-    }
+    } 
 });
